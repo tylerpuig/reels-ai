ce714c01cfbf08d27983f1132d693b67e3729188
tylerpuig <tphockey96@yahoo.com>
2025-02-04 09:54:32 -0600

    supabase auth setup

---
+++ client/app.config.js
@@ -0,0 +1,9 @@
+export default {
+  expo: {
+    extra: {
+      supabaseUrl: process.env.EXPO_PUBLIC_SUPABASE_URL,
+      supabaseAnonKey: process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY,
+      apiUrl: process.env.API_URL,
+    },
+  },
+};
